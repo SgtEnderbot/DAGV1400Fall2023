dc9c8415246a7f73c7d3dbc75973c0c490157d31
Sgt. Enderbot the Legend <143134894+SgtEnderbot@users.noreply.github.com>
2023-09-14 20:43:10 +0000

    Update VariablesAndOperatorsPractice.cs

--- VariablesAndOperatorsPractice.cs
+++ VariablesAndOperatorsPractice.cs
@@ -1,16 +1,27 @@
 using System;
 
-//A list of variables used in the WriteLine and operations.
-int smallNum = 5;
-int bigNum = 100;
-string playerName = "Bob";
-string npcName = "Todd";
-//A few operations printed in the WriteLines.
-int x = smallNum + bigNum;
-int y = bigNum / smallNum;
+	public class Program
+	{
+		public void Main()
+		{
+			//A list of variables used in the WriteLine and operations.
+			
+			int smallNum = 5;
+			int bigNum = 100;
+			string playerName = "Bob";
+			string npcName = "Todd";
+			
+			//A few operations printed in the WriteLines.
+			
+			int x = smallNum + bigNum;
+			int y = bigNum / smallNum;
 
-Console.WriteLine(playerName + " asks " + npcName + " what his favorite numbers are.");
-Console.WriteLine(x);
-Console.WriteLine(y);
-//A WriteLine that includes an operation of the two above operations combined.
-Console.WriteLine(x + y);
+			Console.WriteLine(playerName + " asks " + npcName + " what his favorite numbers are.");
+			Console.WriteLine(x);
+			Console.WriteLine(y);
+			
+			//A WriteLine that includes an operation of the two above operations combined.
+			
+			Console.WriteLine(x + y);
+		}
+	}
