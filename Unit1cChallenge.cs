66b18dbf5ff1e801bcf06ffe5533d2bdced306f3
Sgt. Enderbot the Legend <143134894+SgtEnderbot@users.noreply.github.com>
2023-09-21 17:53:52 -0600

    Added comments.

--- Unit1cChallenge.cs
+++ Unit1cChallenge.cs
@@ -6,18 +6,22 @@ public class Program
     {
         Console.WriteLine("Let's play a guessing game. Guess a number between 1 and 10.");
 		
+        //Here's the int that counts your attempts.
         int attempts = 0;
 
+        //This is where the computer picks a number.
         Random computerGuesser = new Random();
         int value = computerGuesser.Next(1, 11);
 		
-		Console.WriteLine(value);
+		//This is simply to predefine the guess so it can be in this context.
 		int humanGuess = 99;
 
 		do
 		{
+            //Prompts user for a guess.
 			humanGuess = Convert.ToInt32(Console.ReadLine());
 			
+            //Adds to the attempts.
 			attempts ++;
 			
 			if (humanGuess == value)
@@ -34,6 +38,7 @@ public class Program
 				Console.WriteLine("Too low. Guess again.");
 			}
 		}
+        //Re-loops if the guess was incorrect.
 		while (value != humanGuess);    
     }
 }
